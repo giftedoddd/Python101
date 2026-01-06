86b807e89c7cf21254648cdb202e5555ede8d870
giftedodd <giftedoddd@gmail.com>
2026-01-06 17:00:26 +0300

    Add remove_from_list function to helpers.py

--- src/PassGen/src/modules/helpers.py
+++ src/PassGen/src/modules/helpers.py
@@ -5,3 +5,14 @@ def search(name, password_list):
             return creds[3].strip()
     return None
 
+def remove_from_list(name, password_list: list):
+    list_length = len(password_list)
+    index = 0
+    while index < list_length:
+        current_item = password_list[index]
+        username = current_item.split(",")[1]
+        if username == name:
+            password_list.pop(index)
+            return password_list
+
+    return password_list
