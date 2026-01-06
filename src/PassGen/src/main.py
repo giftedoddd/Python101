e15cba90c2aa731e2d8a0073415e0eb6b7bf18e8
giftedodd <giftedoddd@gmail.com>
2026-01-06 17:02:36 +0300

    Add main function to main.py

--- src/PassGen/src/main.py
+++ src/PassGen/src/main.py
@@ -32,3 +32,87 @@ def validate_configs():
     return True
 
 
+def main():
+    print("Welcome to PassGen program.")
+
+    if not validate_configs():
+        print("Failed to validate configuration file.")
+        exit(100)
+
+    print("Enter the operation.")
+    while True:
+        op = input(">> ")
+
+        if op == "help":
+            print_help()
+            continue
+
+        elif op == "exit":
+            print("Goodbye!")
+            exit(0)
+
+        elif op.startswith("create"):
+            input_list = op.split(" ")
+            if not 2 <= len(input_list) <= 3:
+                print("Wrong number of argument.")
+                print_help()
+                continue
+
+            username = input_list[1]
+            if len(input_list) == 3:
+                password = input_list[2]
+            else:
+                password = generate()
+
+            creds = read_file()
+            if search(username, creds):
+                print("This username already saved.")
+                continue
+
+            save_to_file(username, password)
+
+        elif op.startswith("get"):
+            input_list = op.split(" ")
+            if not len(input_list) == 2:
+                print("Wrong number of argument.")
+                print_help()
+                continue
+
+            password_list = read_file()
+            if not password_list:
+                print("Password file is empty, Create a password first.")
+                continue
+
+            username = input_list[1]
+            result = search(username, password_list)
+            if result is None:
+                print("No password found.")
+                continue
+
+            print(result)
+
+        elif op.startswith("remove"):
+            input_list = op.split(" ")
+            if not len(input_list) == 2:
+                print("Wrong number of argument.")
+                print_help()
+                continue
+
+            password_list = read_file()
+            if not password_list:
+                print("Password file is empty, Create a password first.")
+                continue
+
+            username = input_list[1]
+            result = search(username, password_list)
+            if result is None:
+                print("No password found.")
+
+            new_list = remove_from_list(username, password_list)
+            remove_from_file(new_list)
+        else:
+            print("Invalid input try again, Enter help to see help menu.")
+
+
+if __name__ == '__main__':
+    main()
